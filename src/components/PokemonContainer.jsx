dc591778d85e0d345b0cdbe10d9cb96fc1eb2902
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-04-14 10:16:46 +0200

    search added

--- src/components/PokemonContainer.jsx
+++ src/components/PokemonContainer.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PokemonCard from './PokemonCard'
-import {Grid, CircularProgress, TextField} from '@mui/material'
+import {Grid, CircularProgress, TextField, Button, Box} from '@mui/material'
 import "../styles/cardStyles.css"
 import Pagination from './Pagination'
 
@@ -12,6 +12,10 @@ const PokemonContainer = ({
   pokemonsPerPage,
   paginate
 }) => {
+// states
+const [filter, setFilter] = useState('')
+const [filterResult, setFilterResult] = useState([])
+
 // loading condition
   if (loading) {
     return (
@@ -21,30 +25,51 @@ const PokemonContainer = ({
     );
   } 
 
+  // functions
+  const handleSearch = () => {
+    const filteredPokemon = allPokemon.filter(pokemon => pokemon.name.english === `${filter}`);
+    setFilterResult(filteredPokemon);
+  }
+
+
 
   return (
     <>    
       <Grid  className="background" container spacing={2} sx={{display: 'flex', flexDirection: 'column',  alignItems: 'center' }}>
-        <Grid item xs={12}  >
+        <Grid item xs={12}>
           <h1>Play Pokemon</h1>
-          <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-          sx={{bgcolor: 'white', justifySelf: 'center', width: 1}}
-        />
+          <Box sx={{display: 'flex'}} >
+
+            <TextField
+            id="filled-search"
+            label="Search field"
+            type="search"
+            variant="filled"
+            sx={{bgcolor: 'white', justifySelf: 'center', width: 1}}
+            onChange={(event) => setFilter(event.target.value)}
+            onKeyDown={event => event.key === 'Enter' &&  handleSearch()}
+            value={filter}
+            />
+            <Button variant="contained" sx={{m: 0.5, width: 200}} size= 'medium' color='secondary' onClick={() => {setFilter(''); setFilterResult([])}}>Reset filters</Button>
+          </Box>
         </Grid>
         <Grid item xs={10}>
           <Pagination pokemonsPerPage={pokemonsPerPage} numberOfPokemons={allPokemon.length} paginate={paginate}/>
         </Grid>
         <Grid item xs={12}>
           <Grid container spacing={2} sx={{display: 'flex', flexDirection: 'row',  justifyContent: 'center' }}> 
-            {allPokemon && currentPokemons.map(pokemon => 
+            {filterResult.length > 0 ? filterResult.map(pokemon => 
               (<Grid item key={pokemon.id} >
                 <PokemonCard pokemon={pokemon} choosePokemon={choosePokemon} />
               </Grid>)
-            )}
+              )
+              : allPokemon && currentPokemons.map(pokemon => 
+                (<Grid item key={pokemon.id} >
+                  <PokemonCard pokemon={pokemon} choosePokemon={choosePokemon} />
+                </Grid>)
+                )
+              }            
+            
           </Grid>
         </Grid>
       </Grid>
